ae032d0d8f0c4f23f765b6a7ecb1e947495c7960
KR <michal.nowicki1@code.com>
2026-07-10 21:32:55 +0200

    git signs changes

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -56,14 +56,9 @@ return {
       map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[G]it restore % ([R]eset buffer)' })
       map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = '[G]it restore --staged ([u]ndo stage)' })
       map('n', '<leader>gp', gitsigns.preview_hunk, { desc = '[G]it diff ([p]review hunk)' })
-      map('n', '<leader>gi', gitsigns.preview_hunk_inline, { desc = '[G]it diff ([i]nline preview)' })
       map('n', '<leader>gb', function() gitsigns.blame_line { full = true } end, { desc = '[G]it [b]lame line' })
       map('n', '<leader>gd', gitsigns.diffthis, { desc = '[G]it [d]iff (against index)' })
       map('n', '<leader>gD', function() gitsigns.diffthis '@' end, { desc = '[G]it [D]iff HEAD (last commit)' })
-
-      map('n', '<leader>gQ', function() gitsigns.setqflist 'all' end, { desc = '[G]it status ([Q]uickfix all files)' })
-      map('n', '<leader>gq', gitsigns.setqflist, { desc = '[G]it status ([q]uickfix current file)' })
-      map({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
     end,
   },
 }
